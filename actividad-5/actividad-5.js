14e4dac64700fb8fa0c4055f76e8dcd7b3a3002b
Marco Medina <marcos57@hotmail.com>
2022-07-11 20:11:24 -0600

    cambios actividad 7

--- actividad-5/actividad-5.js
+++ actividad-5/actividad-5.js
@@ -1,3 +1,27 @@
 
 
-// Itera el arreglo = [1,4,6,22,55, 46, 2, 5,0]
+// 1.- Itera el arreglo = [1,4,6,22,55, 46, 2, 5,0]
+
+const arr = [1,4,6,10,22,55, 46, 2, 5,0]
+
+for (let i = 0; i < arr.length; i++){
+    if(arr[i] > 3) console.log(`${arr[i]} es mayor a 3`)
+}
+
+// 2.- declara un arreglo vacio con while que se ejecute 5 veces, arega los elmenentos al array
+
+const con = 0;
+const arr2 = [];
+while(cont < 5){
+    arr2[con] = 
+}
+
+
+// 3  - imprimir valores ordenadamente
+
+const arr3 = [1,4,6,10,22,55, 46, 2, 5,0];
+
+for(let i = 0; i < arr3.length; i++){
+    console.log(arr3[i]);
+}
+
---
+++ actividad-7/actividad-7.js
@@ -0,0 +1,72 @@
+
+
+// 1.- reciba dos parametros y retorne true o false si contiene palabra
+
+const contienePalabra = (cadena = "", textoBuscar) => {
+    if (cadena.includes(textoBuscar)){
+        return true
+    }
+
+    return false;
+}
+
+// 2.- encuentra la suma de elementos 
+
+const sumarArreglo = (arreglo = []) => {
+    let total = 0;
+    for(let i = 0; i < arreglo.length; i++){
+        total = total + arreglo[i];
+    }
+    return total;
+}
+
+console.log(sumarArreglo([1,2,3,4,10,11]))
+
+// 3.- desarrolla metodo para verificar si palabra es palindromo 
+const esPalindromo = (palindromo = "") => {
+    if (palindromo === palindromo.split("").reverse().join("")){
+        return true;
+    } 
+    return false;
+}
+
+console.log(esPalindromo("madam"));
+console.log(esPalindromo("computadora"));
+
+// 4 numero de ocurrencias de letra en cadena 
+const ocurrenciasPalabra = (cadena, textoBuscar) => {
+    cadena = cadena.split(textoBuscar);
+
+    return `LA palabra se repite: ${cadena.length - 1} veces`;
+
+
+}
+
+
+// 5 .- valro de arreglo
+
+const mayorArreglo = (arreglo) => {
+    let mayor = arreglo[0];
+    for(let i = 0; i < arreglo.length; i++){
+        if (arreglo[i] > mayor) {
+            mayor = arreglo[i];
+        }
+    }
+    return mayor;
+}
+
+console.log(mayorArreglo([45,56,32,98,1000,5,23]))
+
+
+// 6 .- temperaturas delcius a fahrenheit
+const convertirTemperaturas = (celcius, fahrenheit) => {
+    const celciusAFahrenheit = (celcius * 1.8) + 32;
+
+    const fahrenheitACelcius = (fahrenheit - 32) / 1.8;
+
+    return [celciusAFahrenheit, fahrenheitACelcius]
+}
+
+const respuesta = convertirTemperaturas(32, 65);
+
+console.log(`Celcius a fahrenheit: ${respuesta[0]}, fahrenheit a Celcuus: ${respuesta[1]}`);
